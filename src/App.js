92be5e29d72035ca3ef0edd29b7c9584d808ce87
Aaron Yu <ayybuddyy@gmail.com>
2021-03-03 13:57:52 -0500

    Add Material UI, create async function to grab countries and display in dropdown list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    // async
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className='App'>
-      <h1></h1>
+    <div className='app'>
+      <div className='app__header'>
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value='abc'>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            {/* <MenuItem value='worldwide'>Worldwide</MenuItem>
+            <MenuItem value='worldwide'>Option 2</MenuItem>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            <MenuItem value='worldwide'>Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+      {/* header */}
+      {/* title + select input dropdown field */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* infoboxes */}
+      {/* table */}
+      {/* graph */}
+      {/* map */}
     </div>
   );
 }
